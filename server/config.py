d40863ef4a5c0a31f50c00cdbfb8646048cb46e9
SumitSingh2299 <sumitsinghrajput724@gmail.com>
2025-08-09 13:42:40 +0530

    Update config.py

--- server/config.py
+++ server/config.py
@@ -1,12 +1,11 @@
+# server/config.py
 import os
 from dotenv import load_dotenv
+
+# Load variables from .env file
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'a-default-secret-key'
-
-    # Change this to MONGO_URI to match the production standard
-    MONGO_URI = os.environ.get('MONGO_URI') or "mongodb://localhost:27017/chatsphere"
-
-    FLASK_ENV = os.environ.get('FLASK_ENV')
-    FRONTEND_URLS = os.environ.get('FRONTEND_URLS', '')
+    SECRET_KEY = os.getenv("SECRET_KEY", "dev_key")  
+    MONGO_URI = os.getenv("MONGO_URI")
+    FRONTEND_URLS = os.getenv("FRONTEND_URLS", "")
