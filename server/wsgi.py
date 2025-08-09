34f303c59d3f73fb47b18fb614d6634382f76849
SumitSingh2299 <sumitsinghrajput724@gmail.com>
2025-08-09 13:46:51 +0530

    Update wsgi.py

--- server/wsgi.py
+++ server/wsgi.py
@@ -1,8 +1,6 @@
-from app import create_app
-
+from app import create_app, socketio
 
 app, socketio = create_app()
 
 if __name__ == "__main__":
-    # For local development — use SocketIO’s run method so events work
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+    socketio.run(app, host="0.0.0.0", port=5000)
